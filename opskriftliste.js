075c26357e56f65b3304626c4f5fd4f85eabc1a0
Jules KJ <juljen2730@gmail.com>
2024-02-21 16:44:13 +0100

    footer

--- opskriftliste.js
+++ opskriftliste.js
@@ -48,7 +48,8 @@ function showOpskrifter(opskriftJSON) {
     opskriftClone.querySelector(".month").textContent = opskrift.month;
     opskriftClone.querySelector(".recepie_image").src = opskrift.img;
 
-    // her tjekker jeg i daten og det ekelte opskrift om "herbs" er falske, for hvis den er skal den fjenre urte iconet
+    // her tjekker jeg i daten og det ekelte opskrift om "herbs" er falske, for hvis den er skal
+    //den fjenre urte iconet
     if (opskrift.herbs == false) {
       opskriftClone.querySelector(".herbs").classList.add("none");
     }
